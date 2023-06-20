953b01f59aa06bd7298f526c486cead73c7b51ac
unknown <jamierobertdawson@gmail.com>
2023-06-20 14:59:37 -0700

    moved Navigation tab into it's own component.87

---
+++ src/components/navigation/navigation.componenet.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          {" "}
+          <h1>Logo</h1>
+        </Link>
+
+        <div className="nav-links-contianer">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
