27852acbad9c4ef23e61d9608d0bff214b0b9046
Jamie Dawson <jamierobertdawson@gmail.com>
2024-01-22 15:47:21 -0800

    still getting error

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -1,5 +1,5 @@
-import { createAction } from "../../utils/reducer/reducer.utils";
 import { USER_ACTION_TYPES } from "../user/user.types";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 export const setCurrentUser = (user) => {
   return createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
